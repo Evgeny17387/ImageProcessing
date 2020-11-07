c86e5a0e7b3e084b43432909df96073edae74f1c
Evgeny Ostrovsky <evostrov@microsoft.com>
2020-11-07 11:49:24 +0200

    update

--- HW_1/script.m
+++ HW_1/script.m
@@ -54,14 +54,62 @@ disp(section_a_text)
 
 % pause
 
-%% Section A - Extra
-
-
-
-
-
-
 %% Section B
+
+clc;
+clear;
+close all;
+
+T = 128;
+
+figure('WindowState', 'maximized');
+set(gcf, 'Color', 'white');
+
+image = round(double(imread("Images\ricefields.tif")));
+subplot(1, 3, 1)
+imagesc(image)
+rectangle('Position', [25, 80, 60, 150], 'Curvature', [0.8, 0.4], 'EdgeColor', 'r', 'LineWidth', 4, 'LineStyle', '--')
+title(['Original - 8 bits'], 'FontSize', 16)
+colormap('gray');
+axis image;
+set(gca,'xtick',[])
+set(gca,'ytick',[])
+
+[bimage] = binarize(image, 128);
+subplot(1, 3, 2)
+imagesc(bimage)
+rectangle('Position', [25, 80, 60, 150], 'Curvature', [0.8, 0.4], 'EdgeColor', 'r', 'LineWidth', 4, 'LineStyle', '--')
+title(['Binarized Manually, Threshold = ', num2str(T)], 'FontSize', 16)
+colormap('gray');
+axis image;
+set(gca,'xtick',[])
+set(gca,'ytick',[])
+
+[bimageOpt, T] = binarizeOpt(image);
+subplot(1, 3, 3)
+imagesc(bimageOpt)
+rectangle('Position', [25, 80, 60, 150], 'Curvature', [0.8, 0.4], 'EdgeColor', 'r', 'LineWidth', 4, 'LineStyle', '--')
+title(['Binarized Optimal, Threshold = ', num2str(T)], 'FontSize', 16)
+colormap('gray');
+axis image;
+set(gca,'xtick',[])
+set(gca,'ytick',[])
+
+section_b_text = [
+    '\bf ', 'Section B: Manual Binarization Vs. Optimal Binarization', ' \rm', ...
+    newline, ...
+    'In the red squares, for the manual binarization the person is hard to see, but in the optimized he can be easily distinguished', ...
+    newline, ...
+    'Optimal Binatrization gives better results since it finds a threshold which is between the person and the background, unlike the given threshold which makes them the same', ...
+    newline, ...
+    'Please hit Enter key to continue to the next section'
+];
+sgtitle(section_b_text, 'Color', 'blue')
+disp(section_b_text)
+
+% pause
+
+%% Section C
 
 clc;
 clear;
@@ -111,39 +159,3 @@ section_b_text = [
 ];
 sgtitle(section_b_text, 'Color', 'blue')
 disp(section_b_text)
-
-% pause
-
-%% Section C
-
-clc;
-clear;
-close all;
-
-image = round(double(imread("Images\ricefields.tif")));
-
-[bimage] = binarize(image, 128);
-
-[bimageOpt, T] = binarizeOpt(image);
-
-figure('WindowState', 'maximized');
-
-subplot(1, 3, 1)
-imagesc(image)
-title('8 bit')
-colormap('gray');
-axis image;
-
-subplot(1, 3, 2)
-imagesc(bimage)
-title('1 bit')
-colormap('gray');
-axis image;
-
-subplot(1, 3, 3)
-imagesc(bimageOpt)
-title('1 bit Optimized')
-colormap('gray');
-axis image;
-
-% pause
