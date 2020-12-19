a27a9f21a9bbbdff5236eb92413bd76de8927450
Evgeny Ostrovsky <evostrov@microsoft.com>
2020-12-19 09:45:57 +0200

    add section d

--- HW_3/denoiseScript.m
+++ HW_3/denoiseScript.m
@@ -161,3 +161,92 @@ figure('WindowState', 'maximized');
 set(gcf, 'Color', 'white');
 sgtitle(section_c_text, 'Color', 'blue')
 disp(section_c_text)
+
+%% Section D
+
+clc;
+clear;
+close all;
+
+imageFileName = "lena.tif";
+p = 0.1;
+
+im = round(double(imread("Images\" + imageFileName)));
+
+[noisyIm] = addSPnoise(im, p);
+
+psnrArray = zeros(1, 10);
+radius_index = 0;
+
+figure('WindowState', 'maximized');
+set(gcf, 'Color', 'white');
+
+subplot(1, 4, 1)
+imagesc(im)
+title(['Original'], 'FontSize', 16)
+colormap('gray');
+axis image;
+set(gca,'xtick',[])
+set(gca,'ytick',[])
+
+subplot(1, 4, 2)
+imagesc(noisyIm)
+title(['With SP Noise', 'PSNR: ' + string(calcPSNR(noisyIm, im))], 'FontSize', 16)
+colormap('gray');
+axis image;
+set(gca,'xtick',[]);
+set(gca,'ytick',[]);
+
+for radius = 1 : 10
+
+    radius_index = radius_index + 1;
+    maskRadius = [radius, radius];
+    [cleanImMedian] = cleanImageMedian(noisyIm, maskRadius);
+    psnrArray(radius_index) = calcPSNR(cleanImMedian, im);
+
+    subplot(1, 4, 3)
+    imagesc(cleanImMedian)
+    title(['Median Filter', 'Radisu: ' + string(radius), 'PSNR: ' + string(psnrArray(radius_index))], 'FontSize', 16)
+    colormap('gray');
+    axis image;
+    set(gca,'xtick',[]);
+    set(gca,'ytick',[]);
+
+    subplot(1, 4, 4);
+    plot(psnrArray(1: radius_index));
+    title(['PSNR Vs. Radius'], 'FontSize', 16);
+    xlabel('Radius');
+    ylabel('PSNR');
+
+    section_d_text = [
+        '\bf ', 'Section D: SP Noise, PSNR as Function of Median Filter Radius', ' \rm', ...
+        newline, ...
+        'Radius loop is 1 to 10', ...
+        newline, ...
+        newline, ...
+        newline, ...
+        'Please wait untill simulations ends'
+    ];
+
+    sgtitle(section_d_text, 'Color', 'blue')
+    disp(section_d_text)
+    
+    pause(2);
+
+end
+
+section_d_text = [
+    '\bf ', 'Section D: SP Noise, PSNR as Function of Median Filter Radius', ' \rm', ...
+    newline, ...
+    'As can be seen, the PSNR and the image quality degrades as the radius increases', ...
+    'Which is expected since we take more neighbors thus "ruining" the image quality', ...
+    newline, ...
+    newline, ...
+    newline, ...
+    'Please hit Enter key to continue to the next section'
+];
+
+sgtitle(section_d_text, 'Color', 'blue')
+disp(section_d_text)
+
+pause
